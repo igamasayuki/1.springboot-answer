9477a50678c13b468b851571baa815a76f7f6684
伊賀将之 <igamasayuki@igamasayukinoMacBook-puro-2.local>
2020-06-18 17:31:41 +0900

    Javadocコメント追加

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -14,6 +14,12 @@ import org.springframework.stereotype.Repository;
 
 import com.example.domain.Employee;
 
+/**
+ * employeesテーブルを操作するリポジトリ.
+ * 
+ * @author igamasayuki
+ *
+ */
 @Repository
 public class EmployeeRepository {
 	@Autowired
